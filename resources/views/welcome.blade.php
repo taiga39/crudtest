bdb5d9be151102ba8aca13c708b598a1a4f91e5c
taiga39 <ishikawa.tigers@gmail.com>
2019-02-19 15:24:37 +0900

    2/19 title変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelaaa
                 </div>
 
                 <div class="links">
